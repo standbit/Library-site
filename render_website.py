f89814d7e9feb52582fad127aa34a1ecb3d0fa3e
Stanislav <77130336+standbit@users.noreply.github.com>
2022-10-26 21:54:04 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import json
 import unicodedata
+from livereload import Server
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -13,7 +14,7 @@ def fetch_json(json_file):
     return content
 
 
-def main():
+def on_reload():
     library_content = fetch_json("./books_description.json")
     env = Environment(
         loader=FileSystemLoader("."),
@@ -26,9 +27,28 @@ def main():
     )
     with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
-    
-    server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-    server.serve_forever()
+
+
+def main():
+    # library_content = fetch_json("./books_description.json")
+    # env = Environment(
+    #     loader=FileSystemLoader("."),
+    #     autoescape=select_autoescape(["html"])
+    #     )
+    # template = env.get_template("template.html")
+
+    # rendered_page = template.render(
+    #     library=library_content
+    # # )
+    # with open("index.html", "w", encoding="utf8") as file:
+    #     file.write(rendered_page)
+
+    on_reload()
+    server = Server()
+    server.watch("template.html", on_reload)
+    server.serve(root=".")
+    # server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
+    # server.serve_forever()
 
 
 if __name__ == "__main__":
