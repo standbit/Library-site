65457fc83e6196037b86a996019f2a19f37157ca
Stanislav <yastva3@gmail.com>
2022-11-01 19:29:03 +0300

    Split books by two columns

--- render_website.py
+++ render_website.py
@@ -1,8 +1,8 @@
 import json
 import unicodedata
 from livereload import Server
+from more_itertools import chunked
 
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
@@ -10,12 +10,16 @@ def fetch_json(json_file):
     with open(json_file, "r") as library_file:
         library_json = library_file.read()
     library_json_norm = unicodedata.normalize("NFC", library_json)
-    content = json.loads(library_json_norm)
-    return content
+    library_description = json.loads(library_json_norm)
+    return library_description
 
 
 def on_reload():
-    library_content = fetch_json("./books_description.json")
+
+    library_content = fetch_json(
+        json_file="./books_description.json")
+    col_length = len(library_content)//2 + 1
+    columned_library = list(chunked(library_content, col_length))
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html"])
@@ -23,32 +27,18 @@ def on_reload():
     template = env.get_template("template.html")
 
     rendered_page = template.render(
-        library=library_content
+        library=columned_library
     )
     with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
 
 
 def main():
-    # library_content = fetch_json("./books_description.json")
-    # env = Environment(
-    #     loader=FileSystemLoader("."),
-    #     autoescape=select_autoescape(["html"])
-    #     )
-    # template = env.get_template("template.html")
-
-    # rendered_page = template.render(
-    #     library=library_content
-    # # )
-    # with open("index.html", "w", encoding="utf8") as file:
-    #     file.write(rendered_page)
-
+    
     on_reload()
     server = Server()
     server.watch("template.html", on_reload)
     server.serve(root=".")
-    # server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-    # server.serve_forever()
 
 
 if __name__ == "__main__":
