05eeeab5298e83680a61acec516dbc92231a67bd
Stanislav <yastva3@gmail.com>
2022-11-02 07:26:46 +0300

    Add links to book txt files

--- render_website.py
+++ render_website.py
@@ -5,29 +5,42 @@ from more_itertools import chunked
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+global no_picture_gif
+no_picture_gif = "./images/nopic.gif"
+
 
 def fetch_json(json_file):
     with open(json_file, "r") as library_file:
         library_json = library_file.read()
-    library_json_norm = unicodedata.normalize("NFC", library_json)
+    library_json_norm = unicodedata.normalize("NFKD", library_json)
     library_description = json.loads(library_json_norm)
     return library_description
 
 
-def on_reload():
-
-    library_content = fetch_json(
-        json_file="./books_description.json")
-    col_length = len(library_content)//2 + 1
-    columned_library = list(chunked(library_content, col_length))
+def get_html_template_env():
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html"])
         )
     template = env.get_template("template.html")
+    return template
 
+
+def make_library_in_columns():
+    library_content = fetch_json(
+        json_file="./books_description.json")
+    col_length = len(library_content)//2 + 1
+    columned_library = list(chunked(library_content, col_length))
+    return columned_library
+
+
+def on_reload():
+
+    columned_library = make_library_in_columns()
+    template = get_html_template_env()
     rendered_page = template.render(
-        library=columned_library
+        library=columned_library,
+        no_pic=no_picture_gif,
     )
     with open("index.html", "w", encoding="utf8") as file:
         file.write(rendered_page)
