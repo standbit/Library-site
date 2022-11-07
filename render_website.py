e2f8584450967bde155f96f1113ed3893e8b5a2d
Stanislav <yastva3@gmail.com>
2022-11-07 08:07:14 +0300

    Activate paginator

--- render_website.py
+++ render_website.py
@@ -1,12 +1,11 @@
-from distutils.archive_util import make_archive
 import json
 import unicodedata
-from livereload import Server
-from more_itertools import chunked
-
+from math import ceil
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+from more_itertools import chunked
 
 
 def fetch_json(json_file):
@@ -27,7 +26,8 @@ def get_html_template_env():
 
 
 def make_page_in_columns(page):
-    col_length = len(page)//2
+    columns = 2
+    col_length = ceil(len(page)/columns)
     columned_page = list(chunked(page, col_length))
     return columned_page
 
@@ -35,15 +35,18 @@ def make_page_in_columns(page):
 def on_reload():
     library_content = fetch_json(
         json_file="./books_description.json")
-    paged_library_content = list(chunked(library_content, 10))
+    paged_library_content = list(chunked(library_content, n=10))
+    num_of_pages = len(paged_library_content)
     no_picture_gif = "./images/nopic.gif"
     template = get_html_template_env()
-    for num, page in enumerate(paged_library_content, 1):
+    for num, page in enumerate(paged_library_content, start=1):
         columned_page = make_page_in_columns(page)
+        page_path = str(Path(pages_folder, f"index{num}.html"))
         rendered_page = template.render(
             columned_library_page=columned_page,
-            no_pic=no_picture_gif)
-        page_path = str(Path(pages_folder, f"index{num}.html"))
+            no_pic=no_picture_gif,
+            num_of_pages=num_of_pages,
+            page_num=num)
         with open(page_path, "w", encoding="utf8") as file:
             file.write(rendered_page)
 
@@ -51,7 +54,7 @@ def on_reload():
 def main():
     global pages_folder
     pages_folder = Path("./", "pages")
-    pages_folder.mkdir(parents=True, exist_ok=True)   
+    pages_folder.mkdir(parents=True, exist_ok=True)
     on_reload()
     server = Server()
     server.watch("template.html", on_reload)
