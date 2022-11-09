7eaabbea9cd1fc781357d6f4ad7a5d61aa1b55d0
Stanislav <yastva3@gmail.com>
2022-11-09 12:40:38 +0300

    Add some spaces for readability

--- render_website.py
+++ render_website.py
@@ -9,6 +9,7 @@ from more_itertools import chunked
 
 
 def fetch_json(json_file):
+
     with open(json_file, "r") as library_file:
         library_json = library_file.read()
     normalized_library_json = unicodedata.normalize("NFKD", library_json)
@@ -17,6 +18,7 @@ def fetch_json(json_file):
 
 
 def get_html_template_env():
+
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html"])
@@ -26,21 +28,25 @@ def get_html_template_env():
 
 
 def make_page_in_columns(page):
+
     col_length = ceil(len(page)/2)
     columned_page = list(chunked(page, col_length))
     return columned_page
 
 
 def on_reload():
+
     library_content = fetch_json(
         json_file="./books_description.json")
     paged_library_content = list(chunked(library_content, n=10))
     num_of_pages = len(paged_library_content)
     no_picture_gif = "./images/nopic.gif"
     template = get_html_template_env()
+
     for num, page in enumerate(paged_library_content, start=1):
         columned_page = make_page_in_columns(page)
         page_path = str(Path(pages_folder, f"index{num}.html"))
+
         rendered_page = template.render(
             columned_library_page=columned_page,
             no_pic=no_picture_gif,
